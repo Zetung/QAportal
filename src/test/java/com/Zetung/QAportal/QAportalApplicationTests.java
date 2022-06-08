bb4d90c25aa8540280c3a303a0650fbed3f4cac5
Zetung <v.s.kasperovich@gmail.com>
2022-06-08 11:35:05 +0300

    Init commit

---
+++ src/test/java/com/Zetung/QAportal/QAportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zetung.QAportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QAportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
